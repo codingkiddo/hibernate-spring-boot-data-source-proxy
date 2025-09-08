c08983119dc44ef4ea17671cad3ef950cd5d2bfc
Vinod Kumar <vinodkumar@192.168.1.3>
2025-09-08 12:13:51 +0530

    Initial Commit

---
+++ src/main/java/com/hibernate_spring_boot_data_source_proxy/HibernateSpringBootDataSourceProxyApplication.java
@@ -0,0 +1,13 @@
+package com.hibernate_spring_boot_data_source_proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateSpringBootDataSourceProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateSpringBootDataSourceProxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hibernate_spring_boot_data_source_proxy/HibernateSpringBootDataSourceProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hibernate_spring_boot_data_source_proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateSpringBootDataSourceProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
